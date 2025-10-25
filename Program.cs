337c29ad57e69140be7834ecdfcfe4a72ed85d37
Butters <butters.devs@gmail.com>
2025-10-25 17:48:12 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ public class Program{
     public static void Main(){
         Console.WriteLine("Hello, World!");
         Debug.WriteLine("Hello From Debug");
+
+        var tick = 0;
+        var stillRunning = true;
+
+        do{
+            if(tick == 100)
+            {
+                stillRunning = false
+                Debug.WriteLine("Still running");
+            }
+            else{
+                Debug.WriteLine("Stopped");
+            }
+
+        }
+        while(stillRunning);
     }
 }
 
